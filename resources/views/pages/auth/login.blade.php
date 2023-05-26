9288b1d585f4cf0be2404474251338e78ecfd4d4
Muhamad Azis <88225954+muhamadAzis32@users.noreply.github.com>
2023-05-26 11:01:57 +0700

    Membuat proses login

--- resources/views/pages/auth/login.blade.php
+++ resources/views/pages/auth/login.blade.php
@@ -26,17 +26,32 @@
                         <div
                             class="authentication-login min-vh-100 bg-body row justify-content-center align-items-center p-4">
                             <div class="col-sm-8 col-md-6 col-xl-9">
+
                                 <h2 class="mb-3 fs-7 fw-bolder">Welcome to Modernize</h2>
                                 <p class=" mb-9">Your Admin Dashboard</p>
-                                <form>
+                                <form action="{{ route('login') }}" method="POST">
+                                    @csrf
+
                                     <div class="mb-3">
-                                        <label for="exampleInputEmail1" class="form-label">Username</label>
-                                        <input type="email" class="form-control" id="exampleInputEmail1"
+                                        <label class="form-label">Email address</label>
+                                        <input type="email" name="email" value="{{ old('email') }}"
+                                            class="form-control @error('email') is-invalid @enderror"
                                             aria-describedby="emailHelp">
+                                        @error('email')
+                                            <div class="invalid-feedback">
+                                                {{ $message }}
+                                            </div>
+                                        @enderror
                                     </div>
                                     <div class="mb-4">
-                                        <label for="exampleInputPassword1" class="form-label">Password</label>
-                                        <input type="password" class="form-control" id="exampleInputPassword1">
+                                        <label class="form-label">Password</label>
+                                        <input type="password" name="password"
+                                            class="form-control @error('password') is-invalid @enderror">
+                                        @error('password')
+                                            <div class="invalid-feedback">
+                                                {{ $message }}
+                                            </div>
+                                        @enderror
                                     </div>
                                     <div class="d-flex align-items-center justify-content-between mb-4">
                                         <div class="form-check">
@@ -48,10 +63,14 @@
                                         </div>
                                         <a class="text-primary fw-medium" href="">Forgot Password ?</a>
                                     </div>
-                                    <a href="" class="btn btn-primary w-100 py-8 mb-4 rounded-2">Sign In</a>
+
+
+                                    <button type="submit" class="btn btn-primary w-100 py-8 mb-4 rounded-2">Sign
+                                        In</button>
+
                                     <div class="d-flex align-items-center justify-content-center">
                                         <p class="fs-4 mb-0 fw-medium">New to Modernize?</p>
-                                        <a class="text-primary fw-medium ms-2" href="">Create an account</a>
+                                        <a class="text-primary fw-medium ms-2" href="/register">Create an account</a>
                                     </div>
                                 </form>
                             </div>
